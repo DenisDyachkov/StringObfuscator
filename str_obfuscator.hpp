245d04740fb41a1aaa6c59e4c1336f1bdaf542d3
Denis Dyachkov <denisrafdyachkov@yandex.ru>
2019-07-29 11:52:03 +0700

    Fix calculation max_value

--- str_obfuscator.hpp
+++ str_obfuscator.hpp
@@ -31,7 +31,7 @@ class cryptor {
 public:
 	template<typename T, std::size_t S>
 	class string_encryptor {
-		static constexpr std::size_t max_value = 0xffffffff >> (sizeof(T) << 3);
+		static constexpr std::size_t max_value = 0xffffffff >> ((sizeof(std::size_t) - sizeof(T)) << 3);
 	public:
 		using value_type = T;
 
